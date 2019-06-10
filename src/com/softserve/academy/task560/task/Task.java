e0cd388fa260ec551198a4eb93b5c9ff6a187020
Andrei Rusanovich <a.dovgyj@gmail.com>
2019-06-11 01:59:19 +0300

    fix code styling

--- src/com/softserve/academy/task560/task/Task.java
+++ src/com/softserve/academy/task560/task/Task.java
@@ -5,24 +5,23 @@ import java.util.Map;
 
 public class Task {
 
-    public Map<Integer,Integer> run()
-    {
-        Map<Integer,Integer> numbersSumOfDividers = new HashMap<>();
+    public Map<Integer, Integer> run() {
+        Map<Integer, Integer> numbersSumOfDividers = new HashMap<>();
 
         for (int i = 200; i < 300; i++) {
             int sum = div(i);
-            numbersSumOfDividers.put(i,sum);
+            numbersSumOfDividers.put(i, sum);
 
         }
 
-        Map<Integer,Integer> rez = new HashMap<>();
+        Map<Integer, Integer> rez = new HashMap<>();
 
         int currentNumber = 200;
-        while (currentNumber != 300){
-            for (int i = 200; i < 300;i++){
-                if(i != currentNumber && numbersSumOfDividers.get(i) == currentNumber && numbersSumOfDividers.get(currentNumber) == i){
-                    if(!rez.containsValue(i)){
-                        rez.put(i,currentNumber);
+        while (currentNumber != 300) {
+            for (int i = 200; i < 300; i++) {
+                if (i != currentNumber && numbersSumOfDividers.get(i) == currentNumber && numbersSumOfDividers.get(currentNumber) == i) {
+                    if (!rez.containsValue(i)) {
+                        rez.put(i, currentNumber);
                     }
                 }
             }
@@ -32,11 +31,13 @@ public class Task {
         return rez;
     }
 
-    public  int div(int number){
+    public int div(int number) {
         int sum = 0;
-        for (int i = 1; i < number; i++){
+
+        for (int i = 1; i < number; i++) {
             sum += (number % i) == 0 ? i : 0;
         }
+
         return sum;
     }
 
